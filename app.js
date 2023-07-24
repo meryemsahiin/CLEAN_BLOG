17388f3a9b6bf0a81aa9c643a4c1dbc9c973a850
Meryem Sahin <sahinmeryem620@gmail.com>
2023-07-24 18:58:54 +0300

    ejs and middlewares

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-const express = require('express')
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
+// TEMPLETE ENGINE
+app.set('view engine', 'ejs');
 
-app.use(express.static('public'))
+// MIDDLEWARES
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+  res.render('index');
+});
 
-    const blog = { 
-        id: 1, 
-        title: "Blog title", 
-        description: "Blog description" }
-    res.send(blog)
-})
+app.get('/about', (req, res) => {
+    res.render('about');
+  });
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post');
+  });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Sunucu ${port} portunda çalıştırıldı.`)
-})
+  console.log(`Sunucu ${port} portunda çalıştırıldı.`);
+});
